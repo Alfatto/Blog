67575521b7f29a0e4a442c0e1b43766b7306860f
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-06 18:48:12 +0500

    Update 1.java

--- 1.java
+++ 1.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.lang.Integer;
 public class Level1{
  public static int squirrel(int N){
-        if (N == 0){
-            return 0;
-        }
         int fact = 1;
         while (N > 1){
             fact *= N;
