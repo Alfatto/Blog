4e736d274b05b0f1e6b31277c09ffc069d8042b2
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-06 18:13:48 +0500

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,15 +1,15 @@
 import java.util.*;
-import java.lang.Integer;
-public class Main {
-   if (N == 0){
-           return 0;
-       }
-       int fact = 1;
-       while (N > 1){
-           fact *= N;
-           System.out.println(fact);
-           N--;
-       }
-      return Integer.parseInt(Integer.toString(fact).substring(0,1));
-   }
+public class Level1{
+ public static int squirrel(int N){
+        if (N == 0){
+            return 0;
+        }
+        int fact = 1;
+        while (N > 1){
+            fact *= N;
+            System.out.println(fact);
+            N--;
+        }
+        return Integer.parseInt(Integer.toString(fact).substring(0,1));
+    }
 }
