51c7989338d734a28aa51ec344ed1df0062ca647
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-07 11:42:09 +0500

    Update 2.java

--- Tasks/2.java
+++ Tasks/2.java
@@ -1,11 +1,17 @@
 import java.util.*;
 public class Level1{
   public static int odometer(int [] oksana){
-        int result = 0;
-        for (int i = 0; i < oksana.length; ) {
-            result += oksana[i];
-            i += 2;
+   int result = 0;
+        int time = 0;
+        int time1;
+
+        for (int i = 0; i < oksana.length; i++) {
+            if (i % 2 != 0){
+                time1 = oksana[i] - time;
+                time = oksana[i];
+                result += oksana[i -1] * time1;
+            }
         }
-        return result;
+    return result; 
     }
 }
