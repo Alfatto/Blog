e2be1b2f98516669fe3a5e8c8dd71e562ba90d53
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-10 19:39:25 +0500

    Update 7.java

--- Tasks/7.java
+++ Tasks/7.java
@@ -1,25 +1,24 @@
 import java.util.*;
 
 public class Level1{
-
-public static int [] WordSearch(int len, String s, String subs){
+	public static int[] WordSearch(int len, String s, String subs) {
 		StringBuilder builder = new StringBuilder("");
 
 		int N = 0;
 		int indecs = 0;// для возврата со строки
 		int resultLench = 0;
-		int indecsArray =0;
+		int indecsArray = 0;
 		String[] words = s.split(" ");
 		for (int k = 0; k < words.length; k++) {
 			if (builder.length() + words[N].length() < len) {
 				if (k != words.length - 1) {
 					builder.append(words[N] + '_');
 				} else {
-					builder.append(words[N] + '_');
+					builder.append(words[N]);
 				}
 				N++;
 			} else if (words[N].length() >= 12) {
-				builder.append(words[N]);
+				builder.append(words[N] + '_');
 				N++;
 			} else {
 				for (int i = builder.length(); i < len; i++) {
@@ -29,19 +28,15 @@ public static int [] WordSearch(int len, String s, String subs){
 				k--;
 			}
 		}
-		
-		indecs = s.indexOf(subs);
 
-			logger.info(" Индекс " + indecs );
-			resultLench = builder.length() / 12;
-			indecs = s.indexOf(subs) / 12;
-			logger.info(" Индекс четный " + indecs );
+		resultLench = builder.length() / 12;
+		indecs = s.indexOf(subs) / 12;
 
-		if (builder.length() % 12 == 0){
-			logger.info(" Индекс " + indecs );
+
+		if (builder.length() % 12 == 0) {
 			resultLench += 1;
 			indecs += 1;
-			logger.info(" Индекс не четный " + indecs );
+
 		}
 
 		int[] result = new int[resultLench];
