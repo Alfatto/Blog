0cb27d9cd7e385571fddd7a1a437becc09802229
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-11 19:22:50 +0500

    Update 7.java

--- Tasks/7.java
+++ Tasks/7.java
@@ -1,51 +1,91 @@
 import java.util.*;
 
 public class Level1{
-	public static int[] WordSearch(int len, String s, String subs) {
-		StringBuilder builder = new StringBuilder("");
 
+static boolean podstroka(String A, String B) {
+		String str = " ,.";
+
+		if (B.equals("")) {
+			return true;
+		}
+
+		for (int i = 0; i < A.length() && B.length() <= A.length() - i; i++) {
+			for (int c = 0; c < B.length() && B.charAt(c) == A.charAt(c + i); c++) {
+				if (c == B.length() - 1) {
+					if (B.length() == A.length()) {
+						return true;
+					} else if (c + 1 + i < A.length() && str.contains(Character.toString(A.charAt(c + i + 1)))) {
+						return true;
+					} else if (i == 0 && str.contains(Character.toString(A.charAt(c + i + 1)))) {
+						return true;
+					} else if (i > 0 && str.contains(Character.toString(A.charAt(i - 1)))) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public static int[] WordSearch(int len, String s, String subs) {
 		int N = 0;
-		int indecs = 0;// для возврата со строки
+		//int indecs = 0;// для возврата со строки
 		int resultLench = 0;
 		int indecsArray = 0;
+		String abc = "";
 		String[] words = s.split(" ");
+		StringBuilder builder = new StringBuilder("");
+		final int lenCopy = len;
+
+		if (s.length() != 0) {
+			indecsArray++;
+		}
+		//	int indeksKopy = 0;
+
 		for (int k = 0; k < words.length; k++) {
-			if (builder.length() + words[N].length() < len) {
+
+			if (builder.length() + words[N].length() <= len) {
 				if (k != words.length - 1) {
-					builder.append(words[N] + '_');
+					builder.append(words[N] + ' ');
 				} else {
 					builder.append(words[N]);
 				}
 				N++;
-			} else if (words[N].length() >= 12) {
-				builder.append(words[N] + '_');
+				//	logger.info("builder " + builder);
+			} else if (words[N].length() >= len) {
+				builder.append(words[N] + ' ');
 				N++;
 			} else {
 				for (int i = builder.length(); i < len; i++) {
-					builder.append('?');
+					builder.append(' ');
 				}
-				len += 12;
+				indecsArray++;
+				len += lenCopy;
 				k--;
 			}
 		}
-
-		resultLench = builder.length() / 12;
-		indecs = s.indexOf(subs) / 12;
+		int[] result = new int[indecsArray];
 
 
-		if (builder.length() % 12 == 0) {
-			resultLench += 1;
-			indecs += 1;
-
-		}
-
-		int[] result = new int[resultLench];
-		for (int i : result) {
+		for (int i = 0; i < result.length; i++) {
 			result[i] = 0;
 		}
-		result[indecs] = 1;
+
+		String[] stroke = new String[indecsArray];
+		for (int b = 0, c = lenCopy; b < result.length; b++) {
+			if (b == result.length - 1) {
+				stroke[b] = builder.substring(c - lenCopy, builder.length());
+			} else {
+				stroke[b] = builder.substring(c - lenCopy, c);
+			}
+			c += lenCopy;
+		}
+
+		for (int i = 0; i < stroke.length; i++) {
+			if (podstroka(stroke[i], subs)) {
+				result[i] = 1;
+			}
+		}
 		return result;
-
 	}
-
 }
