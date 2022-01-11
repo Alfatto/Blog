ee7c88eea80c461cdc9e6809bff138708bc8cb3b
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-11 19:26:44 +0500

    Update 7.java

--- Tasks/7.java
+++ Tasks/7.java
@@ -28,8 +28,6 @@ static boolean podstroka(String A, String B) {
 	}
 
 	public static int[] WordSearch(int len, String s, String subs) {
-		int N = 0;
-		//int indecs = 0;// для возврата со строки
 		int N = 0;	
 		int resultLench = 0;
 		int indecsArray = 0;
