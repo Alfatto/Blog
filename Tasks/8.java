efa63b911e90415082cd2ce548f21dbaa4bb1c1d
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-01-13 23:03:15 +0500

    Update 8.java

--- Tasks/8.java
+++ Tasks/8.java
@@ -4,16 +4,16 @@ public class Level1{
  public static int SumOfThe(int N, int [] data){
 		int result = 0;
 		for (int k: data) {
-			for (int i : data) {
-				if (i != k) {
-					result += i;
+			result += k;
+		}
+		for (int i: data){
+			result -= i;
+			for (int r: data){
+				if (result == r){
+					return result;
 				}
 			}
-			if (result == k) {
-				break;
-			}
-			result = 0;
 		}
 		return result;
-	}	
+	}
 }
