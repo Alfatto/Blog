9196b355962eb3dd8004ab923ed836d50d7ec303
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-02-03 13:59:42 +0500

    Create 14.java

---
+++ Tasks/14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Level1{
+	public static int Unmanned(int L, int N, int [][] track){
+		int result = 12;
+
+		/*for (int i = 0; i < L ; i++){
+			if (i == track[0][0]){
+				result += track[0][1];
+			} else {
+				result++;
+			}
+		}*/
+
+		return  result;
+	}
+}
