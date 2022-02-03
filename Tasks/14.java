4d79033ce422a6002fd6e664f277104a96062c27
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-02-03 14:40:29 +0500

    Update 14.java

--- Tasks/14.java
+++ Tasks/14.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 public class Level1{
 	public static int Unmanned(int L, int N, int [][] track){
-		int result = 12;
+		int result = 0;
+		int flag = 0;
 
-		/*for (int i = 0; i < L ; i++){
-			if (i == track[0][0]){
-				result += track[0][1];
+		for (int i = 0; i < L ; i++){
+			if (L < track[0][0]){
+				return  L;
+			} else
+			if (i == track[0][flag] - 1){
+				result += track[1][0] - track[0][0] + 1;
 			} else {
 				result++;
 			}
-		}*/
-
+		}
 		return  result;
 	}
 }
