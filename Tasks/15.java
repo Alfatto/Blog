bd2138606af915170d66c0f2c575f3349857415f
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-02-06 22:00:37 +0500

    Create 15.java

---
+++ Tasks/15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Level1{
+	public static int Unmanned(int L, int N, int [][] track) {
+
+public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2){
+		boolean kart = false;
+		String[] array1;
+		String[] array2;
+
+		array1 = S1.split("\\s");
+		array2 = S2.split("\\s");
+
+		for (String s1: array1) {
+			for (String s2 : array2) {
+				if (s1.contains(s2)){
+					kart = true;
+					break;
+				}
+			}
+		}
+		return kart;
+	}
+
+}
