1f0bb520ea2413a97ec35f28c1ada29506e2b962
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-02-08 16:35:16 +0500

    Update 15.java

--- Tasks/15.java
+++ Tasks/15.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 public class Level1{
 
-public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2){
-		boolean kart = false;
+	public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2){
 		String[] array1;
 		String[] array2;
 
 		array1 = S1.split("\\s");
 		array2 = S2.split("\\s");
 
-		for (String s1: array1) {
-			for (String s2 : array2) {
-				if (s1.contains(s2)){
-					kart = true;
-					break;
+		for (int i = 0; i < array1.length - 1; i++){
+			for (int k = 0; k < array2.length - 1; k++){
+				if (array1[i].indexOf(array2[k]) == array1[i + 1].indexOf(array2[k + 1]) && -1 != array1[i].indexOf(array2[k])){
+					return true;
 				}
 			}
 		}
-		return kart;
+		return false;
 	}
 
 }
