8ddae30232e3f342f2184617535eb8c43715a090
Alfatto <32759262+Alfatto@users.noreply.github.com>
2022-02-12 09:35:11 +0500

    Create 16.java

---
+++ Tasks/16.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Level1{
+	public static int MaximumDiscount(int N, int[] price) {
+		int result = 0;
+		int numberOfDiscountPurchases = 3;
+
+
+		Arrays.sort(price);
+
+		int i;
+		for (i = price.length; i > 0; ) {
+			i -= numberOfDiscountPurchases;
+			if (i >= 0) {
+				result += price[i];
+			}
+		}
+		return result;
+	}
+}
